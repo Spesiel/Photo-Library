0d06540112b722c0c21fc8a9d9c960f6d6aae20a
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-03 02:00:36 -0800

    person => Added ==/!=

--- PhotoLibraryCache/Objects/Person.cs
+++ PhotoLibraryCache/Objects/Person.cs
@@ -8,5 +8,36 @@ namespace PhotoLibrary.Cache.Objects
         public string DisplayName { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Firstname.GetHashCode() ^ Lastname.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Person))
+                return false;
+
+            return Equals((Person)obj);
+        }
+
+        public bool Equals(Person other)
+        {
+            if (Firstname != other.Firstname || Lastname != other.Lastname)
+                return false;
+
+            return true;
+        }
+
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return person1.Equals(person2);
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !person1.Equals(person2);
+        }
     }
 }
